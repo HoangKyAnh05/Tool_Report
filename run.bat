68a1490b5ecd0e9943958f0d64bbade638e6b7ad
Hoàng Kỳ Anh <167224767+HoangKyAnh05@users.noreply.github.com>
2026-08-28 14:57:51 +0700

    chore: clean up process management and update run.bat

--- run.bat
+++ run.bat
@@ -2,16 +2,11 @@
 title Video Reminder
 cd /d "%~dp0"
 
-if not exist "node_modules\electron\dist\electron.exe" (
-    echo [1/2] Dang cai dat thu vien, vui long doi...
-    call npm install
-)
-
+echo [1/2] Dang kiem tra ban build moi nhat...
 if not exist "dist\index.html" (
-    echo [2/2] Dang build ung dung, vui long doi...
     call npm run build
 )
 
-echo Dang khoi chay Video Reminder...
+echo [2/2] Dang khoi chay Video Reminder...
 start "" "node_modules\electron\dist\electron.exe" "%~dp0."
 exit
